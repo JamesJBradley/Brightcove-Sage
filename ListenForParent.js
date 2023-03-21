3e80e7acb684a4a588f6ee87cd974dfe1aa29eb9
Jeremie Ratovoherizo <jeremie.ratovoherizo@sage.com>
2023-03-21 19:03:54 +0100

    Update ListenForParent.js

--- ListenForParent.js
+++ ListenForParent.js
@@ -10,10 +10,7 @@ videojs.registerPlugin('listenForParent', function () {
             event.source.postMessage(message, event.origin);
         } else if ("playerCurrentTime") {
             var currentTime = myPlayer.currentTime();
-            console.log("Current Time:  " + currentTime);
-            var message = "Video CurrentTime";
-            console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + currentTime);
-            event.source.postMessage(message, event.origin);
+            console.log("test update");
         } else if (event.data === "playVideo") {
             myPlayer.play();
         } else if (event.data === 'pauseVideo') {
