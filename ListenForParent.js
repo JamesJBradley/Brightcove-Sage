a8fc72497ee3b29a87e1eeb3ceaf2ceda2e194cb
Jeremie Ratovoherizo <jeremie.ratovoherizo@sage.com>
2023-03-22 14:47:29 +0100

    Update ListenForParent.js

--- ListenForParent.js
+++ ListenForParent.js
@@ -5,28 +5,57 @@ videojs.registerPlugin('listenForParent', function () {
         if (event.data === "playerDetail") {
             console.log('Brightcove: message received from sage.com:  ' + event.data, event);
 
-             myPlayer.on('pause', function (ev) {
+            var message = "holla back youngin!";
+            console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
+            event.source.postMessage(message, event.origin);
+        } else if (event.data === "playerStartTracking") {
+            console.log('Brightcove: message received from sage.com:  ' + event.data, event);
+            var playerEvents = [
+                'ended',
+                'loadeddata',
+                'loadedmetadata',
+                'pause',
+                'play',
+                'ready',
+                'stalled',
+                'timeupdate',
+                'volumechange'
+            ];
+            playerEvents.forEach(function (playerEvent) {
+                myPlayer.on(playerEvent, function (ev) {
+                    var state = ev.type;
+                    var message = {
+                        state: state,
+                        currentTime: this.currentTime(),
+                        duration: this.duration(),
+                        muted: this.muted(),
+                        playbackRate: this.playbackRate(),
+                        videoBufferedPercent: this.bufferedPercent(),
+                        videoId: this.mediainfo.id,
+                        videoName: this.mediainfo.name,
+                        videoTitle: this.mediainfo.name,
+                        videoUrl: this.currentSrc(),
+                        volume: parseInt(this.volume() * 100)
+                    };
+                    console.log('Brightcove:  sending message to sage.com:  ' + " Data: " + JSON.stringify(message));
+                    event.source.postMessage(JSON.stringify(message), event.origin);
+                });
+            });
+        } else if (event.data === "trackingPause") {
+            console.log('Brightcove: message received from sage.com:  ' + event.data, event);
+            myPlayer.on('pause', function (ev) {
                 var message = "pause tracked !!!";
                 alert(message);
                 console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
                 event.source.postMessage(message, event.origin);
-             });
-        } else if (event.data === "trackingPause") {
-            //myPlayer.on('pause', function (ev) {
-              //  var message = "pause tracked !!!";
-                //alert(message);
-                //console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
-                //event.source.postMessage(message, event.origin);
-            //});
+            });
         } else if (event.data === "playVideo") {
             myPlayer.play();
         } else if (event.data === 'pauseVideo') {
             myPlayer.pause();
         }
     };
-    
-    
-    
+
     // Listen for the message, then call controlVideo() method when received
     window.addEventListener("message", controlVideo);
 });
