badd4075c333b9ec3a39f61a5f5c2ff8f38b9be8
Jeremie Ratovoherizo <jeremie.ratovoherizo@sage.com>
2023-03-22 10:58:31 +0100

    Update ListenForParent.js

--- ListenForParent.js
+++ ListenForParent.js
@@ -8,40 +8,6 @@ videojs.registerPlugin('listenForParent', function () {
             var message = "holla back youngin!";
             console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
             event.source.postMessage(message, event.origin);
-        } else if (event.data === "playerStartTracking") {
-            console.log('Brightcove: message received from sage.com:  ' + event.data, event);
-            myPlayer.on('loadstart', function (ev) {
-                var state = ev.type;
-                var message = {
-                    state: state,
-                    currentTime: this.currentTime(),
-                    duration: this.duration(),
-                    muted: this.muted(),
-                    playbackRate: this.playbackRate(),
-                    videoBufferedPercent: this.bufferedPercent(),
-                    videoId: this.mediainfo.id,
-                    videoName: this.mediainfo.name,
-                    videoTitle: this.mediainfo.name,
-                    videoUrl: this.currentSrc(),
-                    volume: parseInt(this.volume() * 100)
-                };
-                console.log('Brightcove:  sending message to sage.com:  ' + " Data: " + JSON.stringify(message));
-                event.source.postMessage(JSON.stringify(message), event.origin);
-            });
-            var playerEvents = [
-                'ended',
-                'loadeddata',
-                'loadedmetadata',
-                'pause',
-                'play',
-                'ready',
-                'stalled',
-                'timeupdate',
-                'volumechange'
-            ];
-            playerEvents.forEach(function (playerEvent) {
-                myPlayer.on(playerEvent, handlePlaybackEvent);
-            });
         } else if (event.data === "trackingPause") {
             console.log('Brightcove: message received from sage.com:  ' + event.data, event);
             myPlayer.on('pause', function (ev) {
