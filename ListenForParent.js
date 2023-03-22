f6f813c5cf265de8169993a372dd9815778e0ac1
Jeremie Ratovoherizo <jeremie.ratovoherizo@sage.com>
2023-03-22 11:24:56 +0100

    Update ListenForParent.js

--- ListenForParent.js
+++ ListenForParent.js
@@ -10,19 +10,27 @@ videojs.registerPlugin('listenForParent', function () {
             event.source.postMessage(message, event.origin);
         } else if (event.data === "trackingPause") {
             console.log('Brightcove: message received from sage.com:  ' + event.data, event);
-            myPlayer.on('pause', function (ev) {
+            //myPlayer.on('pause', function (ev) {
+              //  var message = "pause tracked !!!";
+                //alert(message);
+                //console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
+                //event.source.postMessage(message, event.origin);
+            //});
+        } else if (event.data === "playVideo") {
+            myPlayer.play();
+            myPlayer.on('pause', function {
                 var message = "pause tracked !!!";
                 alert(message);
                 console.log('Brightcove:  sending message to sage.com:  ' + message + " Data: " + JSON.stringify(myPlayer));
                 event.source.postMessage(message, event.origin);
             });
-        } else if (event.data === "playVideo") {
-            myPlayer.play();
         } else if (event.data === 'pauseVideo') {
             myPlayer.pause();
         }
     };
-
+    
+    
+    
     // Listen for the message, then call controlVideo() method when received
     window.addEventListener("message", controlVideo);
 });
